2a2aa05714afb907cd3f6c1e25728eef98d74178
yiseldt <yiseldtmoreno_1993@hotmail.com>
2024-12-19 19:28:03 -0500

    agregando nuevo controler

--- controllers/pokemonControllers.js
+++ controllers/pokemonControllers.js
@@ -27,3 +27,49 @@ exports.getPokemonStatus = async (req, res) => {
         return res.status(500).json({e})
     }
 }
+exports.getPokemonByPokemonId = async (req,res)=>{
+    try {
+        const pokemon_id = req.params.pokemon_id;
+        let pokemon = await statusPokemon.findOne({"pokemon_id":pokemon_id})
+        if(!pokemon){
+            res.status(404).json({message:"Pokemon not found"})
+        }else{
+            res.status(200).json(pokemon)
+        }
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+}
+exports.catchPokemonByPokemon_id = async (req,res)=>{
+    const pokemon_id = req.params.pokemon_id;
+    const pokemonStatusId = req.body.pokemon_id;
+    if(pokemon_id==pokemonStatusId){
+        try {
+            const pokemonStatusView = req.body.view;
+            const pokemonStatusCatch = req.body.catch;
+            let pokemon = await statusPokemon.findOne({"pokemon_id":pokemon_id})
+            if(!pokemon){
+                return res.status(400).json({message:"Bad request, pokemon not view yet"})
+            }else if(pokemon.view != pokemonStatusView) {
+                return res.status(400).json({message:"Bad request, inconsistent data"})
+            }else if(pokemon.catch){
+                return res.status(200).json(pokemon)
+            }else{
+                pokemon = await statusPokemon
+                .findOneAndReplace({"pokemon_id":pokemon_id},{
+                    pokemon_id: pokemon_id,
+                    view: true,
+                    catch: true,
+                }, {new:true}
+            )
+            return res.status(200).json(pokemon)
+            }
+        } catch (error) {
+            console.error(error)
+            return res.status(500).json({error})
+        }
+    }else{
+        return res.status(400).json({message:"bad request, pokemon_id in body diferent pokemon_id in params"})
+    }
+}
--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const {test, createPokemonStatus,getPokemonStatus } = require("../controllers/pokemonControllers")
+const {test, createPokemonStatus,getPokemonStatus, getPokemonByPokemonId, catchPokemonByPokemon_id } = require("../controllers/pokemonControllers")
 const router = express.Router()
 
-router.get("/test",(req,res)=> {
-    console.log("hello desde test")
-    res.send("hola desde routes").status(200)
-})
+// router.get("/test",(req,res)=> {
+//     console.log("hello desde test")
+//     res.send("hola desde routes").status(200)
+// })
 
 router.get("/test", test)
-router.post("/", createPokemonStatus)
 router.get("/",getPokemonStatus)
+router.get("/pokemonid/:pokemon_id", getPokemonByPokemonId)
+router.post("/", createPokemonStatus)
+router.put("/catch/:pokemon_id", catchPokemonByPokemonId)
 module.exports = router
 
