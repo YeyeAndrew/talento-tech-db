33f6ba4edefb04939453f7759d0e910f69ef7a78
YeyeAndrew <yeyeaqe@gmail.com>
2024-12-19 19:56:35 -0500

    tercer commit

--- controllers/pokemonControllers.js
+++ controllers/pokemonControllers.js
@@ -41,7 +41,7 @@ exports.getPokemonByPokemonId = async (req,res)=>{
         return res.status(500).json({error})
     }
 }
-exports.catchPokemonByPokemon_id = async (req,res)=>{
+exports.catchPokemonByPokemonId = async (req,res)=>{
     const pokemon_id = req.params.pokemon_id;
     const pokemonStatusId = req.body.pokemon_id;
     if(pokemon_id==pokemonStatusId){
@@ -71,5 +71,23 @@ exports.catchPokemonByPokemon_id = async (req,res)=>{
         }
     }else{
         return res.status(400).json({message:"bad request, pokemon_id in body diferent pokemon_id in params"})
+    }
+}
+exports.inTeamPokemonByPokemonID = async (req, res) =>{
+    try {
+        const pokemon_id = req.params.pokemon_id
+        const pokemon = await statusPokemon.findOne({"pokemon_id":pokemon_id})
+        const newpokemon = await statusPokemon.findOneAndReplace
+        ({"pokemon_id":pokemon_id},{
+            pokemon_id: pokemon_id,
+            view: true,
+            catch: true,
+            in_team: !pokemon.in_team
+        },{new:true}
+        )
+        return res.status(200).json(newpokemon)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
     }
 }
